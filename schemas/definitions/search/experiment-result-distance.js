eca2fb9e10625f11b37d020995f447daba9e4114
Yassire Elhani <yassire@makeandship.com>
2019-05-31 09:54:01 +0100

    separate distance result and predictor result

---
+++ schemas/definitions/search/experiment-result-distance.js
@@ -0,0 +1,35 @@
+const DistanceResult = {
+  type: "object",
+  title: "Distance Result",
+  properties: {
+    type: {
+      title: "Type",
+      type: "string",
+      enum: ["distance-tree-distance", "distance-nearest-neighbour"]
+    },
+    subType: {
+      title: "SubType",
+      type: "string",
+      enum: ["tree-distance", "nearest-neighbour"]
+    },
+    received: {
+      title: "Received",
+      type: "string",
+      format: "date-time"
+    },
+    analysed: {
+      title: "Analysed",
+      type: "string",
+      format: "date-time"
+    },
+    experiments: {
+      title: "Experiments",
+      type: "array",
+      items: {
+        type: "object"
+      }
+    }
+  }
+};
+
+export { DistanceResult };
